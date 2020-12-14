8118ae7a82b3fd5456032ac0551eb2fcb6ece5d0
Roman Grinev <alexlojhkin@gmail.com>
2020-12-14 10:33:43 +0700

    LZW bugfix

--- src/com/company/LZW.java
+++ src/com/company/LZW.java
@@ -7,17 +7,23 @@ import java.io.IOException;
 import java.util.*;
 
 public class LZW {
+    private final static int CC=0x100;
+    private final static int EOD=0x101;
+
     private int currentCode;
     private int codeLen=9;
-    private int strIndex=0;
+    int maxCode=0;
 
     private List<Integer> str=new ArrayList<>();
-    private Map<List<Integer>, Integer> dictionary=new HashMap<>();
+    private Map<List<Byte>, Integer> dictionary=new HashMap<>();
+    private Map<Integer, List<Byte>> revDictionary=new HashMap<>();
 
     public void encodeFile(String sourceFileName, String destinationFileName) throws IOException {
         int bytesRead = 0;
-        byte[] inBuffer = new byte[128 * 1024];
-        byte[] outBuffer = new byte[256 * 1024];
+        byte[] raw = new byte[64 * 1024];
+        byte[] compressed = new byte[64 * 1024];
+        byte[] outBuffer = new byte[64 * 1024];
+        byte[] decomp = new byte[64 * 1024];
         int outBufferByteLength;
         File in = new File(sourceFileName);
         File out = new File(destinationFileName);
@@ -25,16 +31,24 @@ public class LZW {
         FileOutputStream fos = new FileOutputStream(out);
 
         long sec = System.currentTimeMillis();
-        currentCode=this.initDictionary(dictionary, 8);
-        bytesRead = fis.read(inBuffer);
-        int i=inBuffer[0] & 0xFF;
-        str.add(strIndex++, i);
-        outBufferByteLength = encodeBlock(inBuffer, 1, bytesRead, outBuffer);
-        fos.write(outBuffer, 0, outBufferByteLength);
+        currentCode=this.initDictionary(dictionary, 8)+1;
+        codeLen=9;
+        maxCode=512;
+        bytesRead = fis.read(raw);
+
+        outBufferByteLength = encodeBlock(raw, 0, bytesRead, compressed);
+        currentCode=this.initRevDictionary(revDictionary, 8)+1;
+        codeLen=9;
+        maxCode=512;
+        decomp=decodeBlock(compressed, outBufferByteLength, decomp);
+        fos.write(decomp, 0, 64 * 1024);
+        fos.close();
         while (true) {
-            bytesRead = fis.read(inBuffer);
+            bytesRead = fis.read(raw);
             if (bytesRead <= 0) break;
-            outBufferByteLength = encodeBlock(inBuffer, 0, bytesRead, outBuffer);
+            outBufferByteLength = encodeBlock(raw, 0, bytesRead, outBuffer);
+
+
             fos.write(outBuffer, 0, outBufferByteLength);
             System.out.println(String.format("%.2f kilobytes", (float)outBufferByteLength / 1024));
         }
@@ -49,42 +63,60 @@ public class LZW {
         System.out.println("Compression speed: " + String.format("%.2f kb/s", ((float) in.length() / 1024) / ((float) sec / 1000)));
     }
 
-    private int putBitsToBuffer(byte[] buffer, int bitPos, int value, int valueBitLength) {
-        for (int i = 0; i != valueBitLength; i++) {
-            if ((value & (1<<(valueBitLength-1))) != 0) buffer[bitPos / 8] |= 1 << (7 - (bitPos % 8));
-            else buffer[bitPos / 8] &= ~(1 << (7 - bitPos % 8));
-            value <<= 1;
-            bitPos++;
+    private int putBitsToBuffer(byte[] buffer, int bitPos, int code, int valueBitLength) {
+        int bytePos=bitPos / 8;
+        byte val=buffer[bytePos];
+        int bPos=bitPos % 8;
+        int mask_pos=0;
+        int counter=valueBitLength;
+        while(counter-->0) {
+            val|=(((code & (1 << mask_pos++))!=0) ? 1 : 0) << bPos++;
+            if (bPos==8){
+                bPos=0;
+                buffer[bytePos++]=val;
+                val=0;
+            }
         }
-        return bitPos;
+        buffer[bytePos++]=val;
+        return bitPos+valueBitLength;
     }
 
     private int getBitsFromBuffer(byte[] buffer, int bitPos, int valueBitLength) {
-        int result = 0;
-        for (int i = 0; i != valueBitLength; i++) {
-            result<<=1;
-            if ((buffer[bitPos / 8] & 1 << (7 - bitPos % 8)) != 0)
-                result |= 1;
-            bitPos++;
+        int bytePos=bitPos / 8;
+        int code=0;
+        int val=buffer[bytePos++];
+        bitPos=bitPos % 8;
+        int mask_pos=0;
+        while(valueBitLength-->0) {
+            code|=(((val & (1 << bitPos++))!=0) ? 1 : 0) << mask_pos++;
+            if (bitPos==8){
+                bitPos=0;
+                val=buffer[bytePos++];
+            }
         }
-        return result;
+        return code;
     }
 
-    private int initDictionary(Map<List<Integer>, Integer> dictionary, int maxCodeLen){
+    private int initDictionary(Map<List<Byte>, Integer> dictionary, int maxCodeLen){
         int i;
+        dictionary.clear();
         maxCodeLen=(1<<maxCodeLen);
         for (i=0;i!=maxCodeLen;i++){
-            List<Integer> tmp=new ArrayList<>();
-            tmp.add(i);
-            dictionary.put(tmp, i);
+            ArrayList<Byte> t=new ArrayList<>();
+            t.add(0, (byte)i);
+            dictionary.put(t, i);
         }
         return i;
     }
 
-    private int initDictionary(List<String> dictionary){
+    private int initRevDictionary(Map<Integer, List<Byte>> dictionary, int maxCodeLen){
         int i;
-        for (i=0;i!=255;i++){
-            dictionary.add(i, ""+(char)i);
+        dictionary.clear();
+        maxCodeLen=(1<<maxCodeLen);
+        for (i=0;i!=maxCodeLen;i++){
+            List<Byte> s=new ArrayList<>();
+            s.add(0, (byte)i);
+            dictionary.put(i, s);
         }
         return i;
     }
@@ -100,70 +132,86 @@ public class LZW {
     }
 
     public int encodeBlock(byte inBuffer[], int startIndex, int inBufLength, byte compressedBuffer[]){
-        int sym;
+        byte sym=0;
+        int i=1;
         int outBitPos=0;
-        for (int i=startIndex;i!=inBufLength;i++){
-            sym=inBuffer[i] & 0xFF;
-            str.add(strIndex++, sym);
-            if (!dictionary.containsKey(str)){
-                str.remove(--strIndex);
+        List<Byte> str=new ArrayList<>();
+        str.add(0, inBuffer[0]);
+        while(i!=inBufLength){
+            sym=inBuffer[i++];
+            List<Byte> istr=new ArrayList<>(str);
+            istr.add(istr.size(), sym);
+            if (!dictionary.containsKey(istr)){
                 Integer code=dictionary.get(str);
-                if (code==null){
-                    System.out.println("iter: "+i);
-                }
                 outBitPos=this.putBitsToBuffer(compressedBuffer, outBitPos, code, codeLen);
-                str.add(strIndex++, sym);
-                dictionary.put(new ArrayList<>(str), currentCode++);
-                if (codeLen<getOrder(currentCode)) {
-                    codeLen=getOrder(currentCode);
+                dictionary.put(istr, currentCode++);
+                if (currentCode==maxCode) {
+                    maxCode=1<<(++codeLen);
                     if (codeLen>12) {
-                        outBitPos=this.putBitsToBuffer(compressedBuffer, outBitPos, 1<<codeLen, codeLen);
-                        dictionary.clear();
-                        initDictionary(dictionary, 8);
+                        outBitPos=this.putBitsToBuffer(compressedBuffer, outBitPos, 256, codeLen-1);
+                        currentCode=initDictionary(dictionary, 8)+1;
                         codeLen=9;
+                        maxCode=512;
+                        i--;
+                        str.clear();
+                        str.add(0, inBuffer[i++]);
+                        continue;
                     }
-
                 }
                 str.clear();
-                strIndex=0;
-                str.add(strIndex++, sym);
-            }
+                str.add(0, sym);
+            } else str=istr;
         }
         return (outBitPos+8)/8;
     }
 
-    public int decodeBlock(byte inBuffer[], int inBufLength, String decodedData){
-        int oldCode;
-        int newCode;
+    public byte[] decodeBlock(byte inBuffer[], int inBufLength, byte decodedData[]){
         int inBitLength=inBufLength*8;
-        String str="";
-        char sym;
-        int currentCode;
-        int codeLen=9;
         int inBitPos=0;
-        List<String> dictionary=new ArrayList<>();
-        currentCode=this.initDictionary(dictionary);
-        oldCode=getBitsFromBuffer(inBuffer, inBitPos, codeLen);
+        int outBytePos=0;
+        int newCode;
+        int oldCode=getBitsFromBuffer(inBuffer, inBitPos, codeLen)  & 0x0FFF;
         inBitPos+=codeLen;
-        sym=(char)oldCode;
-        decodedData=""+sym;
+        int sym=oldCode;
+        decodedData[outBytePos++]=(byte)sym;
         while (inBitPos<inBitLength){
-            newCode=getBitsFromBuffer(inBuffer, inBitPos, codeLen);
-            inBitPos+=codeLen;
-            if (dictionary.size()>newCode){
-                str=dictionary.get(newCode);
-            } else {
-                str=dictionary.get(oldCode);
-                str=str+sym;
+            if (currentCode+1==maxCode)
+                maxCode=1<<++codeLen;
+            if (codeLen>12) {
+                newCode = getBitsFromBuffer(inBuffer, inBitPos, codeLen - 1) & 0x0FFF;
+                inBitPos+=codeLen-1;
             }
-            decodedData=decodedData+str;
-            sym=str.charAt(0);
-            dictionary.add(currentCode++, dictionary.get(oldCode)+sym);
-            if (codeLen<getOrder(currentCode+1))
-                codeLen=getOrder(currentCode+1);
+            else {
+                newCode = getBitsFromBuffer(inBuffer, inBitPos, codeLen) & 0x0FFF;
+                inBitPos += codeLen;
+            }
+            if(newCode==256){
+                currentCode=initRevDictionary(revDictionary, 8)+1;
+                maxCode=512;
+                codeLen=9;
+                oldCode=getBitsFromBuffer(inBuffer, inBitPos, codeLen)  & 0x0FFF;
+                inBitPos+=codeLen;
+                sym=oldCode;
+                decodedData[outBytePos++]=(byte)sym;
+                continue;
+            }
+            List<Byte> str;
+            if (revDictionary.containsKey(newCode)){
+                str=new ArrayList<>(revDictionary.get(newCode));
+            } else {
+                str=new ArrayList<>(revDictionary.get(oldCode));
+                str.add(str.size(), (byte)sym);
+            }
+            for (int i=0;i!=str.size();i++)
+                decodedData[outBytePos++]=str.get(i);
+            sym=str.get(0);
+            List<Byte> t=new ArrayList<>(revDictionary.get(oldCode));
+            t.add(t.size(), (byte)sym);
+            revDictionary.put(currentCode++, t);
+
             oldCode=newCode;
         }
-        return 0;
+        return decodedData;
     }
 
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,18 +5,36 @@ import java.util.List;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static byte[] hexStringToByteArray(String s) {
+
+        s = s.replaceAll("\\s+","");
+        int len = s.length();
+        byte[] data = new byte[len / 2];
+        for (int i = 0; i < len; i += 2) {
+            data[i / 2] = (byte) ((Character.digit(s.charAt(i), 16) << 4)
+                    + Character.digit(s.charAt(i+1), 16));
+        }
+        return data;
+    }
+
+    public static void main(String[] args) throws IOException {
         Huffman huffman = new Huffman();
+        byte[] test=hexStringToByteArray("00 A7 65 93 F6 6A 1A B6 69 AF A4 65 4B 75 30 E1 C2 86 0A 19 22 8C 88 ED D5 C2 6C AF B0 A5 CA E6 EA 62 C7 54 74 EA 10 A1 F3 86 0E 9D 22 74 DC D4 A1 43 A4 8E 1B 3A FB 62 6A CC 36 D3 15 C2 86 38 6F EA CC 78 F3 D5 AB 54 1D 5D F9 E4 98 2A E3 50 57 2E E9 2C 71 A3 A4 E4 C9 94 75 94 B8 59 1A 73 DA C7 8A 56 B3 65 65 58 F0 6A D7 87 05 8D 62 B4 88 CA A2 59 8E 08 87 68 28 A9 32 E5 52 A5 2F 5D AE DC 77 71 26 CE 8D 33 EB 6E 7C F8 10 21 46 82 D9 50 61 B4 29 74 A3 50 22 49 88 28 11 F9 F4 E9 54 A6 53 F7 75 24 88 D0 EA C4 BE 36 ED 4A 4C 68 D1 66 67 B3 1D CD 56 CC 48 84 C8 90 91 4E D9 32 96 9B D2 2F C1 86 03 C9 62 DC 3A 33 23 DE 8D AF 3C 07 C6 C8 55 70 58 C4 A5 D5 3E 6D 1A 72 E4 D2 C5 93 2D 66 15 BC 9B 2F C3 BF CE 2B 56 1C 3C 76 34 46 8E AF 4E 03 57 42 E4 65 49 A9 24 A1 BA");
+        byte[] output=new byte[4096];
+        byte[] decompressed=new byte[4096];
+        byte[] dataIn;
+        String s="TOBEORNOTTOBEORTOBEORNOT";
+        dataIn=s.getBytes();
         LZW lzw=new LZW();
-        String dataIn="";
-        String dataOut="";
+        lzw.encodeFile("C:\\Users\\exp10\\Desktop\\LAND2.BMP", "C:\\Users\\exp10\\Desktop\\LAND2.BMP.LZW");
+//        lzw.encodeBlock(dataIn, 0, dataIn.length, output);
+   //     lzw.decodeBlock(test, test.length, decompressed);
         /*try {
             lzw.encodeFile("C:/Users/exp10/Desktop/LAND2..BMP", "C:/Users/exp10/Desktop/LAND2.BMP.COMP");
         }
         catch (IOException e) {
             e.printStackTrace();
         }*/
-        List<Integer> s;
         try {
                             huffman.encodeFile(args[0], args[1]);
                          //   huffman.decodeFile(args[3], args[1] + ".out");
